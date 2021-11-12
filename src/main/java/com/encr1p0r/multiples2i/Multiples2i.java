af8324bdbfaebadb9ababb115dab6c9b9d946d3b
Encr1pt0r <k1802797@kingston.ac.uk>
2021-11-12 14:58:07 +0000

    Solution completed

--- src/main/java/com/encr1p0r/multiples2i/Multiples2i.java
+++ src/main/java/com/encr1p0r/multiples2i/Multiples2i.java
@@ -9,7 +9,7 @@ package com.encr1p0r.multiples2i;
  *
  * @author presi
  */
-public class Main {
+public class Multiples2i {
 
     /**
      * @param args the command line arguments
@@ -18,13 +18,18 @@ public class Main {
         // TODO code application logic here
         
         // Multiples of 7 until 100
+        count(7, 1);
         // Multiples of 8 until 200
+        count(8, 2);
         // Multiples of 9 until 300
+        count(9, 3);
     }
     
     public static void count(int base, int level) {
+        int cur = base;
         for(int i = 0; i < level*100; i++) {
-            System.out.println(base);
+            System.out.println(cur);
+            cur = cur + base;
         }
     }
 }
